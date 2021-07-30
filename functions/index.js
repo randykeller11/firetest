e14503840676161bfe95e15965b6fbaf68738ba4
Randy <randykeller1193@gmail.com>
2021-07-30 11:03:53 -0400

    foundation for business logic

--- functions/index.js
+++ functions/index.js
@@ -2,22 +2,15 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.logActivities = functions.firestore
-    .document("/{collection}/{id}")
+exports.updateLikeCount = functions.firestore
+    .document("pendingUpdates/{id}")
     .onCreate((snap, context) => {
-      console.log(snap.data());
-
-      const activities = admin.firestore().collection("activities");
-
-      const stores = admin.firestore().collection("stores");
-      const collection = context.params.collection;
-
-      if (collection === "pendingUpdates") {
-        return stores.add({text: "new pending update!"});
-      }
-      if (collection === "posts") {
-        return activities.add({text: "someone just made a post"});
-      }
-
-      return null;
+      return admin
+          .firestore()
+          .collection("watchlists")
+          .doc(`${context.params.id}`)
+          .get()
+          .then((doc) => {
+            console.log("Got rule: " + doc.data().test);
+          });
     });
