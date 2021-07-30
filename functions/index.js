2a89388fffcf71c3f0a8f86899849712f5c52d11
Randy <randykeller1193@gmail.com>
2021-07-30 08:55:31 -0400

    es lint

--- functions/index.js
+++ functions/index.js
@@ -3,21 +3,21 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.logActivities = functions.firestore
-  .document("/{collection}/{id}")
-  .onCreate((snap, context) => {
-    console.log(snap.data());
+    .document("/{collection}/{id}")
+    .onCreate((snap, context) => {
+      console.log(snap.data());
 
-    const activities = admin.firestore().collection("activities");
+      const activities = admin.firestore().collection("activities");
 
-    const stores = admin.firestore().collection("stores");
-    const collection = context.params.collection;
+      const stores = admin.firestore().collection("stores");
+      const collection = context.params.collection;
 
-    if (collection === "pendingUpdates") {
-      return stores.add({ text: "new pending update!" });
-    }
-    if (collection === "posts") {
-      return activities.add({ text: "someone just made a post" });
-    }
+      if (collection === "pendingUpdates") {
+        return stores.add({text: "new pending update!"});
+      }
+      if (collection === "posts") {
+        return activities.add({text: "someone just made a post"});
+      }
 
-    return null;
-  });
+      return null;
+    });
