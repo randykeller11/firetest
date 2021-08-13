8e8f94a4a3e7f1000a43d21575c75ac1e485e682
Randy <randykeller1193@gmail.com>
2021-08-12 21:36:26 -0400

    new function for user sign up

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,26 @@
+const { v4: uuidv4 } = require("uuid");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.updateLikeCount = functions.firestore
-    .document("pendingUpdates/{id}")
-    .onCreate((snap, context) => {
-      return admin
-          .firestore()
-          .collection("watchlists")
-          .doc(`${context.params.id}`)
-          .get()
-          .then((doc) => {
-            console.log("Got rule: " + doc.data().test);
-          });
-    });
+  .document("pendingUpdates/{id}")
+  .onCreate((snap, context) => {
+    return admin
+      .firestore()
+      .collection("watchlists")
+      .doc(`${context.params.id}`)
+      .get()
+      .then((doc) => {
+        console.log("Got rule: " + doc.data().test);
+      });
+  });
+
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+  console.log("user created", user.email, user.uid);
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(`${user.uid}`)
+    .set({ profileID: uuidv4() });
+});
