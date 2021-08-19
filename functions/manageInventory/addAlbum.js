d33561515509485d9f9900c6f6c8c832a29acde4
Randy <randykeller1193@gmail.com>
2021-08-19 17:01:51 -0400

    connected updateAlbumPage export function

--- functions/manageInventory/addAlbum.js
+++ functions/manageInventory/addAlbum.js
@@ -1,117 +1,83 @@
-const {v4: uuidv4} = require("uuid");
 const helpers = require("./helperFunctions");
 
-exports.handler = async (db, inventoryUpdateDoc, context) => {
+exports.updateAlbumPage = async (db, inventoryUpdateDoc, context) => {
   const IUDInfo = inventoryUpdateDoc.value;
+  const docID = IUDInfo.albumData.docID;
+  const albumPageSnap = await db.collection("albumPages").doc(`${docID}`).get();
 
-  // add album to users music inventory
-  await db
-      .collection("musicInventories")
-      .doc(`${inventoryUpdateDoc.seller}`)
-      .set({[inventoryUpdateDoc.inventoryID]: IUDInfo}, {merge: true});
-
-  // check album pages collection for a page with audioDB-ID
-  const albumPageRef = db.collection("albumPages");
-  const snapshot = await albumPageRef
-      .where("albumInfo.idAlbum", "==", IUDInfo.albumData.idAlbum)
-      .get();
-
-  // declare important variables for function below
-  const crcInventoryID = uuidv4();
-  const dataForCleanup = {...inventoryUpdateDoc, albumPage: crcInventoryID};
-
-  // if there is not an album page
-  if (snapshot.empty) {
-    await db
-        .collection("albumPages")
-        .doc(`${crcInventoryID}`)
-        .set({
-          albumTitle: IUDInfo.albumData.strAlbum.toLowerCase(),
-          artist: IUDInfo.albumData.strArtist.toLowerCase(),
-          albumInfo: IUDInfo.albumData,
-          formatTags: IUDInfo.formatTags,
-          priceInfo: {
-            [`${IUDInfo.condition}`]: {
-              lowestPrice: IUDInfo.priceTarget,
-              medianPrice: IUDInfo.priceTarget,
-              highestPrice: IUDInfo.priceTarget,
-              totalCopies: 1,
+  const invItemData = albumPageSnap.data();
+  const updateTarget = `${IUDInfo.condition}`;
+  const hasPriceInfo = Object.prototype.hasOwnProperty.call(
+      invItemData.priceInfo,
+      updateTarget,
+  );
+  // console.log(hasPriceInfo);
+  // if no price info exists for this condition add new info with total of 1
+  if (!hasPriceInfo) {
+    db.collection("albumPages")
+        .doc(`${docID}`)
+        .set(
+            {
+              priceInfo: {
+                [`${IUDInfo.condition}`]: {
+                  lowestPrice: IUDInfo.priceTarget,
+                  medianPrice: IUDInfo.priceTarget,
+                  highestPrice: IUDInfo.priceTarget,
+                  totalCopies: 1,
+                },
+              },
             },
-          },
-
-          inAudioDB: true,
-        });
-    helpers.cleanUp(db, dataForCleanup, `${context.params.id}`);
+            {
+              merge: true,
+            },
+        );
+    helpers.cleanUp(
+        db,
+        {...inventoryUpdateDoc, albumPage: docID},
+        `${context.params.id}`,
+    );
     return;
   }
-  // if there is an album page
-  // update album page document to reflect new price
-  snapshot.forEach((_albumPageDoc) => {
-    const invItemData = _albumPageDoc.data();
-    const updateTarget = `${IUDInfo.condition}`;
-    const hasPriceInfo = Object.prototype.hasOwnProperty.call(
-        invItemData.priceInfo,
-        updateTarget,
+  // if there is price info for the condition
+  // update the lowestPrice, highestPrice and medianPrice
+  if (hasPriceInfo) {
+    const _priceInfo = invItemData.priceInfo[updateTarget];
+    const updateObject = {..._priceInfo};
+
+    if (_priceInfo.lowestPrice > IUDInfo.priceTarget) {
+      updateObject.lowestPrice = IUDInfo.priceTarget;
+    }
+
+    if (_priceInfo.highestPrice < IUDInfo.priceTarget) {
+      updateObject.highestPrice = IUDInfo.priceTarget;
+    }
+
+    const [newTotal, newMedian] = helpers.calcMedian(
+        Number(IUDInfo.priceTarget),
+        Number(_priceInfo.medianPrice),
+        Number(_priceInfo.totalCopies),
     );
-    // if no price info exists for this condition add new info with total of 1
-    if (!hasPriceInfo) {
-      db.collection("albumPages")
-          .doc(`${_albumPageDoc.id}`)
-          .set(
-              {
-                priceInfo: {
-                  [`${IUDInfo.condition}`]: {
-                    lowestPrice: IUDInfo.priceTarget,
-                    medianPrice: IUDInfo.priceTarget,
-                    highestPrice: IUDInfo.priceTarget,
-                    totalCopies: 1,
-                  },
-                },
+
+    updateObject.medianPrice = newMedian;
+    updateObject.totalCopies = newTotal;
+
+    db.collection("albumPages")
+        .doc(`${docID}`)
+        .set(
+            {
+              priceInfo: {
+                // note the square brackets
+                [updateTarget]: updateObject,
               },
-              {
-                merge: true,
-              },
-          );
-      helpers.cleanUp(db, dataForCleanup, `${context.params.id}`);
-      return;
-    }
-    // if there is price info for the condition
-    // update the lowestPrice, highestPrice and medianPrice
-    if (hasPriceInfo) {
-      const _priceInfo = invItemData.priceInfo[updateTarget];
-      const updateObject = {..._priceInfo};
-
-      if (_priceInfo.lowestPrice > IUDInfo.priceTarget) {
-        updateObject.lowestPrice = IUDInfo.priceTarget;
-      }
-
-      if (_priceInfo.highestPrice < IUDInfo.priceTarget) {
-        updateObject.highestPrice = IUDInfo.priceTarget;
-      }
-
-      const [newTotal, newMedian] = helpers.calcMedian(
-          Number(IUDInfo.priceTarget),
-          Number(_priceInfo.medianPrice),
-          Number(_priceInfo.totalCopies),
-      );
-
-      updateObject.medianPrice = newMedian;
-      updateObject.totalCopies = newTotal;
-
-      db.collection("albumPages")
-          .doc(`${_albumPageDoc.id}`)
-          .set(
-              {
-                priceInfo: {
-                  // note the square brackets
-                  [updateTarget]: updateObject,
-                },
-              },
-              {
-                merge: true,
-              },
-          );
-      helpers.cleanUp(db, dataForCleanup, `${context.params.id}`);
-    }
-  });
+            },
+            {
+              merge: true,
+            },
+        );
+    helpers.cleanUp(
+        db,
+        {...inventoryUpdateDoc, albumPage: docID},
+        `${context.params.id}`,
+    );
+  }
 };
--- functions/manageInventory/inventoryUpdate.js
+++ functions/manageInventory/inventoryUpdate.js
@@ -1,9 +1,17 @@
 const addAlbum = require("./addAlbum");
 
-
 exports.handler = async (snap, context, db) => {
   const inventoryUpdateDoc = snap.data();
   if (inventoryUpdateDoc.type === "add") {
-    addAlbum.handler(db, inventoryUpdateDoc, context);
+    await db
+        .collection("musicInventories")
+        .doc(`${inventoryUpdateDoc.seller}`)
+        .set(
+            {[inventoryUpdateDoc.inventoryID]: inventoryUpdateDoc.value},
+            {merge: true},
+        );
+    if (inventoryUpdateDoc.value.infoSource === "albumPages") {
+      addAlbum.updateAlbumPage(db, inventoryUpdateDoc, context);
+    }
   }
 };
