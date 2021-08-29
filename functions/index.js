35c262bbcc3d68e750fede19846ed8d52deb99a5
Randy <randykeller1193@gmail.com>
2021-08-29 10:01:03 -0400

    last working prototype

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
 const inventoryUpdate = require("./manageInventory/inventoryUpdate");
-const { v4: uuidv4 } = require("uuid");
+const {v4: uuidv4} = require("uuid");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
 exports.inventoryUpdate = functions.firestore
-  .document("pendingInventoryUpdates/{id}")
-  .onCreate((_snap, _context) => {
-    inventoryUpdate.handler(_snap, _context, db);
-  });
+    .document("pendingInventoryUpdates/{id}")
+    .onCreate((_snap, _context) => {
+      inventoryUpdate.handler(_snap, _context, db);
+    });
 // onUpdate function for "pendingInventoryUpdates/{id}"
 // create copy of PIU object in "crcMusicInventory"
 // add albumPage value to "musicInventories/{id}"
@@ -18,21 +18,21 @@ exports.inventoryUpdate = functions.firestore
 exports.newUserSignUp = functions.auth.user().onCreate((user) => {
   console.log("user created", user.email, user.uid);
   return admin
-    .firestore()
-    .collection("users")
-    .doc(`${user.uid}`)
-    .set({ profileID: uuidv4() });
+      .firestore()
+      .collection("users")
+      .doc(`${user.uid}`)
+      .set({profileID: uuidv4()});
 });
 
 exports.newStoreSignUp = functions.firestore
-  .document("users/{id}")
-  .onUpdate((snap, context) => {
-    const after = snap.after.data();
-    if (after.accountType === 1) {
-      return admin
-        .firestore()
-        .collection("musicInventories")
-        .doc(`${context.params.id}`)
-        .set({});
-    }
-  });
+    .document("users/{id}")
+    .onUpdate((snap, context) => {
+      const after = snap.after.data();
+      if (after.accountType === 1) {
+        return admin
+            .firestore()
+            .collection("musicInventories")
+            .doc(`${context.params.id}`)
+            .set({});
+      }
+    });
