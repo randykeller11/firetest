de6f17b3c2c00e916ffc17755d3e7a52507754b6
Randy <randykeller1193@gmail.com>
2021-09-02 14:27:34 -0400

    new architecture using discogs ID for filtering

--- functions/manageInventory/helperFunctions.js
+++ functions/manageInventory/helperFunctions.js
@@ -1,12 +1,30 @@
-exports.cleanUp = async (db, dataToCopy, docID) => {
-  delete dataToCopy.type;
-  await db.collection("CRCMusicInventory").add(dataToCopy);
-  await db.collection("pendingInventoryUpdates").doc(docID).delete();
-  return;
-};
+exports.albumPagePriceUpdate = (_type, _priceTarget, _priceInfo) => {
+  if (_type === "add") {
+    const calcMedian = (_pT, _currMedian, _currTotal) => {
+      const _newTotal = _currTotal + 1;
+      const _newMedian = (_currTotal * _currMedian + _pT) / _newTotal;
+      console.log(_pT, _currMedian, _currTotal);
+      return [_newTotal, _newMedian];
+    };
 
-exports.calcMedian = (_priceTarget, _currMedian, _currTotal) => {
-  const _newTotal = _currTotal + 1;
-  const _newMedian = (_currTotal * _currMedian + _priceTarget) / _newTotal;
-  return [_newTotal, _newMedian];
+    const updateObject = {..._priceInfo};
+
+    if (_priceInfo.lowestPrice > _priceTarget) {
+      updateObject.lowestPrice = _priceTarget;
+    }
+
+    if (_priceInfo.highestPrice < _priceTarget) {
+      updateObject.highestPrice = _priceTarget;
+    }
+
+    const [newTotal, newMedian] = calcMedian(
+        Number(_priceTarget),
+        Number(_priceInfo.medianPrice),
+        Number(_priceInfo.totalCopies),
+    );
+
+    updateObject.medianPrice = newMedian;
+    updateObject.totalCopies = newTotal;
+    return updateObject;
+  }
 };
--- functions/manageInventory/inventoryUpdate.js
+++ functions/manageInventory/inventoryUpdate.js
@@ -1,51 +1,32 @@
-const addAlbum = require("./addAlbum");
 const {v4: uuidv4} = require("uuid");
+const helpers = require("./helperFunctions");
 
 exports.handler = async (snap, context, db) => {
-  const inventoryUpdateDoc = snap.data();
-  const IUDInfo = inventoryUpdateDoc.value;
-  const dispEssentials = {
-    albumTitle: IUDInfo.albumData.title,
-    artist: IUDInfo.albumData.artists_sort,
-    image: IUDInfo.masterImage,
-    year: IUDInfo.albumData.year,
-    labels: IUDInfo.albumData.labels,
-    format: IUDInfo.albumData.formats,
-    genre: IUDInfo.albumData.genres,
-  };
+  try {
+    const inventoryUpdateDoc = snap.data();
+    const IUDInfo = inventoryUpdateDoc.value;
+    const dispEssentials = {
+      albumTitle: IUDInfo.albumData.title,
+      artist: IUDInfo.albumData.artists_sort,
+      image: IUDInfo.masterImage,
+      year: IUDInfo.albumData.year,
+      labels: IUDInfo.albumData.labels,
+      format: IUDInfo.albumData.formats,
+      genre: IUDInfo.albumData.genres,
+    };
 
-  const priceEssentials = {
-    priceTarget: IUDInfo.priceTarget,
-    mediaCondition: IUDInfo.mediaCondition,
-    sleeveCondition: IUDInfo.sleeveCondition,
-  };
+    const priceEssentials = {
+      priceTarget: IUDInfo.priceTarget,
+      mediaCondition: IUDInfo.mediaCondition,
+      sleeveCondition: IUDInfo.sleeveCondition,
+    };
 
-  const inventoryObject = {
-    dispEssentials: dispEssentials,
-    priceEssentials: priceEssentials,
-  };
+    const inventoryObject = {
+      dispEssentials: dispEssentials,
+      priceEssentials: priceEssentials,
+    };
 
-  if (inventoryUpdateDoc.type === "add") {
-    if (inventoryUpdateDoc.value.infoSource === "albumPages") {
-      await db
-          .collection("musicInventories")
-          .doc(`${inventoryUpdateDoc.seller}`)
-          .set(
-              {
-                [inventoryUpdateDoc.inventoryID]: {
-                  ...inventoryObject,
-                  albumPage: inventoryUpdateDoc.value.albumData.docID,
-                },
-              },
-              {merge: true},
-          );
-      addAlbum.updateAlbumPage(
-          db,
-          inventoryUpdateDoc,
-          context,
-          inventoryObject,
-      );
-    } else {
+    if (inventoryUpdateDoc.type === "add") {
       await db
           .collection("musicInventories")
           .doc(`${inventoryUpdateDoc.seller}`)
@@ -61,23 +42,130 @@ exports.handler = async (snap, context, db) => {
         IUDInfo.sleeveCondition < IUDInfo.mediaCondition ?
           IUDInfo.sleeveCondition :
           IUDInfo.mediaCondition;
-      await db
+
+      const albumPageRef = await db
           .collection("albumPages")
-          .doc(`${crcInventoryID}`)
-          .set({
-            albumTitle_Search: IUDInfo.albumData.title.toLowerCase(),
-            artist_Search: IUDInfo.albumData.artists_sort.toLowerCase(),
-            dispEssentials: inventoryObject.dispEssentials,
-            priceInfo: {
-              [`${conditionGrade}`]: {
-                lowestPrice: IUDInfo.priceTarget,
-                medianPrice: IUDInfo.priceTarget,
-                highestPrice: IUDInfo.priceTarget,
-                totalCopies: 1,
+          .where("releaseID", "==", IUDInfo.albumData.id)
+          .get();
+
+      if (albumPageRef.empty) {
+        await db
+            .collection("albumPages")
+            .doc(`${crcInventoryID}`)
+            .set({
+              albumTitle_Search: IUDInfo.albumData.title.toLowerCase(),
+              artist_Search: IUDInfo.albumData.artists_sort.toLowerCase(),
+              releaseID: IUDInfo.albumData.id,
+              dispEssentials: inventoryObject.dispEssentials,
+              priceInfo: {
+                [`${conditionGrade}`]: {
+                  lowestPrice: IUDInfo.priceTarget,
+                  medianPrice: IUDInfo.priceTarget,
+                  highestPrice: IUDInfo.priceTarget,
+                  totalCopies: 1,
+                },
               },
-            },
-          });
-      return;
+            });
+        const updatedInvObject = {
+          ...inventoryObject,
+          albumPage: `${crcInventoryID}`,
+        };
+        await db
+            .collection("musicInventories")
+            .doc(`${inventoryUpdateDoc.seller}`)
+            .set(
+                {
+                  [inventoryUpdateDoc.inventoryID]: updatedInvObject,
+                },
+                {merge: true},
+            );
+        await db.collection("CRCMusicInventory").add(updatedInvObject);
+
+        return;
+      } else {
+        const docID = albumPageRef.docs[0].id;
+        const invItemData = albumPageRef.docs[0].data();
+
+        const conditionGrade =
+          IUDInfo.sleeveCondition < IUDInfo.mediaCondition ?
+            IUDInfo.sleeveCondition :
+            IUDInfo.mediaCondition;
+        const updateTarget = `${conditionGrade}`;
+        const hasPriceInfo = Object.prototype.hasOwnProperty.call(
+            invItemData.priceInfo,
+            updateTarget,
+        );
+
+        // if no price info exists for this condition
+        // add new info with total of 1
+        if (!hasPriceInfo) {
+          db.collection("albumPages")
+              .doc(`${docID}`)
+              .set(
+                  {
+                    priceInfo: {
+                      [`${conditionGrade}`]: {
+                        lowestPrice: IUDInfo.priceTarget,
+                        medianPrice: IUDInfo.priceTarget,
+                        highestPrice: IUDInfo.priceTarget,
+                        totalCopies: 1,
+                      },
+                    },
+                  },
+                  {
+                    merge: true,
+                  },
+              );
+
+          return;
+        }
+        // if there is price info for the condition
+        // update the lowestPrice, highestPrice and medianPrice
+        if (hasPriceInfo) {
+          const _priceInfo = invItemData.priceInfo[updateTarget];
+          const updateObject = helpers.albumPagePriceUpdate(
+              "add",
+              IUDInfo.priceTarget,
+              _priceInfo,
+          );
+
+          db.collection("albumPages")
+              .doc(`${docID}`)
+              .set(
+                  {
+                    priceInfo: {
+                      // note the square brackets
+                      [updateTarget]: updateObject,
+                    },
+                  },
+                  {
+                    merge: true,
+                  },
+              );
+        }
+
+        const updatedInvObject = {
+          ...inventoryObject,
+          albumPage: `${docID}`,
+        };
+
+        await db
+            .collection("musicInventories")
+            .doc(`${inventoryUpdateDoc.seller}`)
+            .set(
+                {
+                  [inventoryUpdateDoc.inventoryID]: updatedInvObject,
+                },
+                {merge: true},
+            );
+        await db.collection("CRCMusicInventory").add(updatedInvObject);
+      }
     }
+    await db
+        .collection("pendingInventoryUpdates")
+        .doc(`${context.params.id}`)
+        .delete();
+  } catch (error) {
+    console.log(error);
   }
 };
